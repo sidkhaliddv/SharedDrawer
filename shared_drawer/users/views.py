d73dd3209c69e0fda3819d0f4aed3c55c37467ff
sidkhaliddv <sidkhaliddv@gmail.com>
2024-07-21 01:04:25 +0500

    added user registeration functionality

---
+++ shared_drawer/users/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+# Create your views here.
+
+def register(request):
+	if (request.method == 'POST'):
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+	else:
+		form = UserCreationForm()
+	context = {
+		'form': form
+	}
+
+	return render(request, 'users/register.html', context)
