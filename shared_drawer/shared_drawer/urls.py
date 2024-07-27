7ff739ebfacd6fb0b8286e91fe1b191131bb2dd5
sidkhaliddv <sidkhaliddv@gmail.com>
2024-07-27 16:07:40 +0500

    added login logout and landing page functionality

--- shared_drawer/shared_drawer/urls.py
+++ shared_drawer/shared_drawer/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
+	path('admin/', admin.site.urls),
+	path('users/', include('users.urls')),
+	path('', include('dashboard.urls')),
+	path('auth/', include('django.contrib.auth.urls'))
 ]
